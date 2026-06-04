6d808c98684377832bbe2ef1e5c6fb0b945ab6f3
fodTaher10 <foadtaher26@gmail.com>
2026-06-05 01:55:10 +0300

    Add smooth mobile menu animation

--- javaScript/js.js
+++ javaScript/js.js
@@ -1,8 +1,32 @@
 const menuBtn = document.querySelector(".menu-toggle");
 const navbar = document.querySelector(".navbar");
 
-if (menuBtn && navbar) {
-    menuBtn.addEventListener("click", () => {
-        navbar.classList.toggle("active");
+menuBtn.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+
+    navbar.classList.toggle("active");
+
+});
+
+navbar.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+
+});
+
+document.addEventListener("click", () => {
+
+    navbar.classList.remove("active");
+
+});
+
+document.querySelectorAll(".navbar a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navbar.classList.remove("active");
+
     });
-}
+
+});
